d1f91629a9c10e788276eba29efa26ea740a1ecd
Martin <skydiver@users.noreply.github.com>
2016-08-17 11:59:23 -0300

    added revision column

--- models/Revision.php
+++ models/Revision.php
@@ -12,6 +12,12 @@
             'model' => 'array',
         ];
 
+        public function beforeCreate() {
+            $rev  = self::select('revision')->where('post_id', $this->model['id'])->orderBy('revision', 'desc')->first();
+            $next = ($rev) ? $rev->revision + 1 : 1;
+            $this->revision = $next;
+        }
+
     }
 
 ?>
--- updates/create_revisions_table.php
+++ updates/create_revisions_table.php
@@ -11,9 +11,10 @@
 
             Schema::create('rainlab_blog_revisions', function($table) {
                 $table->increments('id')->unsigned();
-                $table->integer('post_id')->unsigned()->nullable();
-                $table->foreign('post_id')->references('id')->on('rainlab_blog_posts')->onDelete('CASCADE');
-                $table->json   ('model');
+                $table->integer('revision')->unsigned();
+                $table->integer('post_id' )->unsigned()->nullable();
+                $table->foreign('post_id' )->references('id')->on('rainlab_blog_posts')->onDelete('CASCADE');
+                $table->json   ('model'   );
                 $table->timestamps();
             });
 
